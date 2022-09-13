311639e29e45a6bc0852543c354468787a3972ae
karlsminton <ksm1nn0@gmail>
2022-09-13 22:41:38 +0100

    get category creation and saving working

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,12 +1,27 @@
 class CategoryController < ApplicationController
   def index; end
 
-  def view
-    pass unless params.include?(:url_key)
-    key = params[:url_key]
-    category = Category.find_by(url: key)
-    return redirect_to({ action: :view, controller: :product, url_key: key })
+  def new
+    @category = Category.new
+  end
 
-    @category = category
+  def save
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category #{@category.name} saved correctly"
+    else
+      flash[:alert] = @category.errors.full_messages
+    end
+    redirect_to action: :new
+  end
+
+  def view
+    @category = Category.find_by(url: params[:url_key])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:id, :name, :url, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html
 
   # Front End Routes
   # Homepage
   root 'home#index'
+
   # Categories
   get '/categories', to: 'category#index'
+  get '/category/new', to: 'category#new'
   get '/category/:url_key', to: 'category#view', as: 'category'
+  post '/category/save', to: 'category#save'
+
   # Products
   get '/product/:url_key', to: 'product#view', as: 'product'
+
   # Customer
   get '/customer/account', to: 'customer#account'
   get '/customer/register', to: 'customer#register'
@@ -23,11 +30,13 @@ Rails.application.routes.draw do
   get '/admin/login', to: 'admin#login'
   get '/admin/create', to: 'admin#create'
   post '/admin/save', to: 'admin#save'
+
   # Manage Products
   get '/admin/product', to: 'product#list'
   get '/admin/product/new', to: 'product#new'
   get '/admin/product/edit/:id', to: 'product#edit', as: 'admin_product_edit'
   post '/admin/product/create', to: 'product#create'
+
   # Cart Routes
   post '/cart/add', to: 'cart#add'
 end
