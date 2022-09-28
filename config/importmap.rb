4fce2df9a0eeff4134ffd5906ed8d1ddb29e16ad
karlsminton <ksm1nn0@gmail>
2022-09-28 22:00:13 +0100

    include jquery - got minicart working

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/uikit", under: "uikit"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
